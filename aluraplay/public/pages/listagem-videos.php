f44d6e02e77306ceaa77d997d3ca7e732ac61b12
Vinicius Rodrigues da Silva <125066078+ViniGHub@users.noreply.github.com>
2023-06-30 11:19:19 -0300

    Arquivos do projeto adicionados ao repositório.

---
+++ aluraplay/public/pages/listagem-videos.php
@@ -0,0 +1,34 @@
+<?php
+$dbPath = "../banco.sqlite";
+$pdo = new PDO("sqlite:$dbPath");
+$videoList = $pdo->query('SELECT * FROM videos;')->fetchAll(PDO::FETCH_ASSOC);
+shuffle($videoList);
+
+?>
+<?php require_once './pages/cabecalho/inicio-html.php'; ?>
+    <ul class="videos__container" alt="videos alura">
+        <?php 
+        foreach ($videoList as $video) { 
+            if (!str_starts_with($video['url'], 'http')) {
+                $video['url'] = 'https://www.youtube.com/embed/dQw4w9WgXcQ';
+                $video['title'] = 'Rick Astley - Never Gonna Give You Up (Official Music Video)';
+            }
+            ?>
+        <li class="videos__item">
+            <iframe width="100%" height="72%" src="<?php echo $video['url']; ?>" title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img src="./img/logo.png" alt="logo canal alura">
+                <h3><?php echo $video['title']; ?></h3>
+                <div class="acoes-video">
+                    <a href="./enviar-video?id=<?=$video['id']; ?>">Editar</a>
+                    <a href="../remover-video?id=<?= $video['id'];?>">Excluir</a>
+                </div>
+            </div>
+        </li>
+        <?php 
+        } ?>
+    </ul>
+    <?php require_once './pages/cabecalho/fim-html.php'; ?>
---
+++ aluraplay/public/pages/login.php
@@ -0,0 +1,23 @@
+<?php require_once './pages/cabecalho/inicio-html.php'; ?>
+    <main class="container">
+
+        <form class="container__formulario">
+            <h2 class="formulario__titulo">Efetue login</h3>
+                <div class="formulario__campo">
+                    <label class="campo__etiqueta" for="usuario">Usuário</label>
+                    <input name="user" class="campo__escrita" required
+                        placeholder="Digite seu usuário" id='usuario' />
+                </div>
+
+
+                <div class="formulario__campo">
+                    <label class="campo__etiqueta" for="senha">Senha</label>
+                    <input type="password" name="senha" class="campo__escrita" required placeholder="Digite sua senha"
+                        id='senha' />
+                </div>
+
+                <input class="formulario__botao" type="submit" value="Entrar" />
+        </form>
+
+    </main>
+<?php require_once './pages/cabecalho/fim-html.php'; ?>
