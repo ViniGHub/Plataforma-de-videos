250e94d26c058e4cdf6ef262e457bd53b6ee5924
Vinicius Rodrigues da Silva <vinicius.rodrigues2004@gmail.com>
2023-06-30 12:03:55 -0300

    Testando o git pelo vs code.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a66d8b127b4eb2bf8a4c727e9d574f100e78e3eb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a66d8b127b4eb2bf8a4c727e9d574f100e78e3eb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
