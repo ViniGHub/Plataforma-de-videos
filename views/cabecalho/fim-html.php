8414c4759f9bd6c2559c1318c69b5da42cf62eb9
Vinicius Rodrigues da Silva <vinicius.rodrigues2004@gmail.com>
2023-07-12 15:47:57 -0300

    Adicionando tela de carregamento.

--- views/cabecalho/fim-html.php
+++ views/cabecalho/fim-html.php
@@ -1,10 +1,13 @@
 <script>
     let load = document.querySelector('#load__page');
-    let body =document.querySelector('body');
+    let body = document.querySelector('body');
 
-    window.addEventListener('load', function () {
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    window.addEventListener('load', async() => {
+        await delay(500);
         load.style.display = 'none';
         body.style.overflow = 'visible';
+
     });
 </script>
 </body>
